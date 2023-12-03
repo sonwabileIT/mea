61f40e1b0aa5a25632ffe4f22272a6f3ec3a398e
Sonwabile <sonwabilegxoyiyait@gmail.com>
2023-12-03 18:41:36 +0200

    add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
