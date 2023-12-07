8f1a4f93eb9128eac6bc133eac68c4dae7c669d8
Sonwabile <sonwabilegxoyiyait@gmail.com>
2023-12-07 14:22:05 +0200

    add navigation bar

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, NavbarComponent],
   templateUrl: './pages.component.html'
 })
 export class PagesComponent {
