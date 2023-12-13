884b72399c626641d424c5577d395e7f6aad9776
Sonwabile <sonwabilegxoyiyait@gmail.com>
2023-12-13 16:26:11 +0200

    feat: add enroll page and routing

--- src/app/pages/page-routes.ts
+++ src/app/pages/page-routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { EnrollComponent } from "./enroll/enroll.component";
 
 const routeConfig: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Home Page'
+    },
+    {
+        path: 'enroll',
+        component: EnrollComponent,
+        title: 'Enroll Page'
     }
 ];
 export default routeConfig;
