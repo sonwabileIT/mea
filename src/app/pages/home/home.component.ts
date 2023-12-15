732e098cd5bb640826184e444dbb54dc1f0990a3
Sonwabile <sonwabilegxoyiyait@gmail.com>
2023-12-15 17:22:18 +0200

    feat: add button component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
