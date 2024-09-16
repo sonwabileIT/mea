e322818c5d8083117c662e28fec53d382cc544c6
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-09-16 17:52:27 +0200

    feat: add module service. Populate modulelist

--- src/app/pages/my-modules/my-modules.component.ts
+++ src/app/pages/my-modules/my-modules.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { IModule } from 'src/app/models/imodule';
+import { CommonModule } from '@angular/common';
+import { ModuleService } from 'src/app/services/module.service';
 
 @Component({
   selector: 'app-my-modules',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-modules.component.html'
 })
 export class MyModulesComponent {
+
+  // async getAllModules(): Promise<IModule[]>{
+  //   const result = await fetch('http://localhost:4000/modules')
+  //   const modules = await result.json();
+  //   return modules ?? [];
+  // }
+
+  constructor(private moduleService: ModuleService){}
+
+  ngOnInit(): void{
+    this.getModules();
+  }
+
+  async getModules() {
+    this.moduleList = await this.moduleService.getAllModules()
+  }
+
+   moduleList: IModule[] = [];
 
 }
